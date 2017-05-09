86394155ca9c5b3d90e656d0593875b97cdac624
achujelas123 <achujel3@gmail.com>
2017-05-09 20:44:26 +0300

    added some CRUD methods

--- src/main/java/io/javatutorials/springboot/topic/TopicController.java
+++ src/main/java/io/javatutorials/springboot/topic/TopicController.java
@@ -1,10 +1,12 @@
 package io.javatutorials.springboot.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -13,9 +15,19 @@ public class TopicController {
     @Autowired
     private TopicService topicService;
 
-    @RequestMapping("/topic")
+    @RequestMapping("/topics")
     public List<Topic> getTopics() {
         return topicService.getTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id) {
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
     }
 
 }
--- src/main/java/io/javatutorials/springboot/topic/TopicService.java
+++ src/main/java/io/javatutorials/springboot/topic/TopicService.java
@@ -2,19 +2,28 @@ package io.javatutorials.springboot.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("spring", "Spring Framework", "Spring Framework description"),
             new Topic("java", "Core Java", "Core Java Description"),
             new Topic("javascript", "Javascript", "Javascript description")
-    );
+    ));
 
     public List<Topic> getTopics() {
         return topics;
+    }
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
     }
 }
